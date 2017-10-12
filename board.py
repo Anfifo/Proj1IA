d7217f1caacb7c3c98ee56ffd3521df1511d2937
leonor-loureiro <leonor.filipa@tecnico.ulisboa.pt>
2017-10-12 16:24:27 +0100

    same_game class altered

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+from search import *
+
 # TAI color
 # sem cor = 0
 # com cor > 0
@@ -36,9 +38,10 @@ class Board:
         Constructor
         :param board:
         """
-        self.board = board
+        self.board = []
         self.nr_lines = len(board)
-        self.nr_columns = len(board)
+        self.nr_columns = len(board[0])
+
 
     def __str__(self):
         """
@@ -52,6 +55,15 @@ class Board:
             buff += '\n'
         return buff
 
+    def get_dimensions(self):
+        return self.nr_lines , self.nr_columns
+
+    def get_board(self):
+        return  self.board
+
+    def __eq__(self, other):
+        return self.board == other.get_board()
+
     def find_groups(self):
         """
         :return: A list with all the groups in the board
@@ -60,12 +72,7 @@ class Board:
         nr_lines = self.nr_lines
 
         groups = []
-        visited = []
-        for i in range(nr_lines):
-            l = []
-            for j in range(nr_columns):
-                l.append(False)
-            visited.append(l)
+        visited = [[False]* nr_columns] * nr_lines
 
         for i in range(nr_lines):
             for j in range(nr_columns):
@@ -73,10 +80,8 @@ class Board:
                     group = self._find_group(make_pos(i, j))
                     for x, y in group:
                         visited[x][y] = 'True'
-                    if len(group) > 1:
-                        groups.append(group)
+                    groups.append(group)
         return groups
-
 
     def _adjacent(self, pos):
         """
@@ -127,19 +132,28 @@ class sg_stage:
         Constructor
         :param board:
         """
-        self.board = board;
+        self.board = Board(board)
+        self.groups = self.board.find_groups()
+
+    def get_board(self):
+        return self.board
+
+    def get_groups(self):
+        return self.groups
+
+    def __eq__(self, other):
+        return self.board == other.get_board()
+
     def __lt__(self, other):
         """
         Less than operator
         :param other: stage to compare to
         :return: true if self < other, false otherwise
         """
-        return
-
-
-class Problem:
-    def __init__(self):
         pass
+
+    def __str__(self):
+        return str(self.board)
 
 
 class same_game(Problem):  # class <class_name>(<super_class>):
@@ -148,16 +162,23 @@ class same_game(Problem):  # class <class_name>(<super_class>):
     A solution cannot have pieces left on the board.
     """
     def __init__(self, board):
-        pass
+        self.initial = sg_stage(board)
+        nr_lines, nr_columns = self.initial.get_board().get_dimensions()
+        self.goal = sg_stage([[0] * nr_columns] * nr_lines)
 
     def actions(self, state):
-        pass
+        actions = []
+        for group in state.get_groups():
+            if len(group) > 1:
+                actions.append(group)
+        return actions
 
     def result(self, state, action):
-        pass
+        return sg_stage(board_remove_group(state.get_board(),action))
+
 
     def goal_test(self, state):
-        pass
+        return state == self.goal
 
     def path_cost(self, c, state1, action, state2):
         pass
@@ -199,15 +220,32 @@ def board_remove_group(b, group):
     print(b)
     return board
 
+"""
+    b = Board([[0, 2, 2], [0, 9, 2], [0, 1, 2]])
+    print(b)
 
-b = Board([[0, 2, 2], [0, 9, 2], [0, 1, 2]])
-print(b)
+    print('------------')
 
-print('------------')
+    groups = board_find_groups([[0, 2, 2], [0, 9, 2], [0, 1, 2]])
+    print(groups)
 
-groups = board_find_groups([[0, 2, 2], [0, 9, 2], [0, 1, 2]])
-print(groups)
+    print('-----------')
 
-print('-----------')
+    board_remove_group(b, groups[1])
 
-board_remove_group(b, groups[0])
+    s = same_game([[0, 2, 2], [0, 9, 2], [0, 1, 2]])
+    state = sg_stage([[0, 2, 2], [0, 9, 2], [0, 1, 2]])
+    print (s.goal_test(state))
+    actions = s.actions(state)
+    print (actions)
+    state2 = s.result(state,actions[0])
+    print(state2)
+    print("state")
+    print(state)
+"""
+
+b1 = Board([[1,2],[1,1]])
+print(b1)
+g = b1.find_groups()
+board_remove_group(b1,g[0])
+print(b1)
